a90c86f0ab1051ab55fd04839c942a7c7d266dca
FaultyTwo <mapcast42@gmail.com>
2021-10-29 12:35:41 +0700

    Update 4KHz-CLK-Driver.ino

--- examples/4KHz-CLK-Driver/4KHz-CLK-Driver.ino
+++ examples/4KHz-CLK-Driver/4KHz-CLK-Driver.ino
@@ -1,3 +1,13 @@
+/* 4KHz-CLK-Driver
+ * Author: Chawin 'FaultyTwo' Treesugol
+ * Date: 9/12/2021
+ * FROM: https://github.com/FaultyTwo/LTC6904-arduino-lib
+ * 
+ * Desc: This is a very barebone usage example of this library
+ * This will output 4kkHZ pulse signal (also known as tinnitus simulator).
+ * 
+ */
+
 #include <LTC6904.h>
 
 LTC6904 dvc(0); //0x16
@@ -7,6 +17,6 @@ void setup() {
   dvc.setFreq(4.0,3);
 }
 
-void loop() { //hello, i am useless
+void loop() {
 
 }
